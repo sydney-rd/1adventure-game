9bd7ead1a363eaabed490174de1f2e428a0c9263
sydney-rd <sydneydavid56@gmail.com>
2023-03-24 13:36:11 -0400

    adding proper time delay for both introduction msg and game start message

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
 // Get the element to be animated
 const gameStartIntro = document.querySelector(".game-start-intro");
-
+const gameIntroduction = document.querySelector(".introduction");
 // Add a class to show the element with a fade-in animation after 5 seconds
 setTimeout(() => {
   gameStartIntro.classList.add("show");
-}, 5000);
+}, 8000);
+
 
 // After 8 seconds, add a class to hide the element with a fade-out animation
 setTimeout(() => {
-  gameStartIntro.classList.add("hide");
+  gameIntroduction.classList.add("hide");
 }, 8000);
 
 // After the fade-out animation is complete, remove the element from the DOM
-gameStartIntro.addEventListener("transitionend", () => {
-  gameStartIntro.parentNode.removeChild(gameStartIntro);
+gameIntroduction.addEventListener("transitionend", () => {
+  gameIntroduction.parentNode.removeChild(gameIntroduction);
 });
