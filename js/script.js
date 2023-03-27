f648b39bbf56a19a9f8afcc11492f4016f2804b0
sydney-rd <sydneydavid56@gmail.com>
2023-03-27 11:08:29 -0400

    finishing one path of story

--- js/script.js
+++ js/script.js
@@ -27,63 +27,75 @@ gameIntroduction.addEventListener("transitionend", () => {
 
 function startQues(e) {
     if (e.target.classList.contains('ans1')) {
-        message = "wrong. "; 
+        main.innerHTML = message; // keep to reset message to empty string
     } else {
-         message = ""; // display whatever msg you want
+        main.innerHTML = message; // keep to reset message to empty string
     }
-    message+= "this is reality. you are now floating in the boundless, three-dimensional continuum where objects can have relative positions, directions and size - aka space";
-    main.innerHTML = message;
-    question.innerText = "float towards the light or towards the darkness";
+    question.innerText = "You are indefintely floating in space. Float towards the light or towards the darkness";
     ans1.innerText = "light"
     ans2.innerText = "dark"
     bothAns.forEach(btn => btn.removeEventListener("click", startQues));
     // document.body.style.background = `black`;
-    ans1.addEventListener('click', lightFunc)
+    ans1.addEventListener('click', exploreLightFunc)
     message = "";
     //ans2.addEventListener("click", darkFunc) 
 }
 
-function lightFunc(e) {
+function exploreLightFunc(e) {
     question.innerText = "you veer towards the first speck of light in the vast darkness and render unconcious, you awake in a seemingly different dimension. You find yourself in a room full of mirrors. Do you..."
     if (e.target.classList.contains("ans1")) {
-        message = "you explore the mirror parallel"; // doesn't show up
-        main.innerHTML = message;
-
+        main.innerHTML = message; // keep to reset message to empty string
     } else {
-        message = "break the mirrors to escape";
-        main.innerHTML = message;
-
+        main.innerHTML = message; // keep to reset message to empty string
     }
     ans1.innerText = "explore";
     ans2.innerText = "escape"
-    bothAns.forEach(btn => btn.removeEventListener("click", lightFunc)); // removes click event
-    ans1.addEventListener("click", exploreFunc);
+    bothAns.forEach(btn => btn.removeEventListener("click", exploreLightFunc)); // removes click event
+    ans1.addEventListener("click", exploreMirrorFunc);
     message = ""; // removes messag
 }
 
-function exploreFunc(e) {
+function exploreMirrorFunc(e) {
     question.innerText = "you explore the boundless reflections as you lose track of time. You don't realize you've been searching for a way out for months, you see yourself and your past through the reflections in your prior life. You barely recognize yourself in the mirrors. Do you...";
     main.innerHTML = message;
     if (e.target.classList.contains("ans1")) {
-        message = "you want to break through the glass"
+        main.innerHTML = message; // keep to reset message to empty string
     } else {
-        message = "you lie hopeless"
+        main.innerHTML = message; // keep to reset message to empty string
     }
     ans1.innerText = "break the mirror";
     ans2.innerText = "becomed filled with despair";
-    bothAns.forEach(btn => btn.removeEventListener("click", lightFunc));
-    ans1.addEventListener("click", breakFunc);
+    bothAns.forEach(btn => btn.removeEventListener("click", exploreLightFunc));
+    ans1.addEventListener("click", breakMirrorFunc);
     message = "";
 }
 
-function breakFunc(e) {
+function breakMirrorFunc(e) {
     question.innerText = "you smash through and instantly are transported back to something familiar...this looks like my childhood home"
     main.innerHTML = message;
     if (e.target.classList.contains("ans1")) {
-        message = "this is a trick..."
+        main.innerHTML = message; // keep to reset message to empty string
     } else {
-        message = "i've woken up from this nightmare!!"
+        main.innerHTML = message; // keep to reset message to empty string
     }
+    ans1.innerText = "this obviously isn't real";
+    ans2.innerText = "I've finally awoken from this nightmare!"
+    ans1.addEventListener("click", childHomeFunc);
+    message = "";
+}
+
+function childHomeFunc(e) {
+    question.innerText = "you walk through your home and observe. Sinks are working, fridge is filled with food. You begin to relax. Later on, you hear a door open. Do you..."
+    main.innerHTML = message;
+    if (e.target.classList.contains("ans1")) {
+        main.innerHTML = message;
+    } else {
+        main.innerHTML = message;
+    }
+    ans1.innerText = "see who it is!";
+    ans2.innerText = "hide!";
+    ans1.addEventListener("click", startQues);
+    message = "";
 }
 
 bothAns.forEach(btn => btn.addEventListener("click", startQues));
