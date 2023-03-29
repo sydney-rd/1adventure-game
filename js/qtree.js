66134c3a8b68d7152ebf6dbff9fe776f7bfea7bd
sydney-rd <sydneydavid56@gmail.com>
2023-03-29 07:48:47 -0400

    deleting files, adding new code, glitch effects

--- js/qtree.js
+++ js/qtree.js
@@ -14,7 +14,6 @@ class Screen {
         this.ans1 = a1;
         this.ans2 = a2;
         this.backgroundSize = bgSize;
-
     }
 }
 
@@ -25,13 +24,23 @@ class Answer {
     }
 }
 
+// // if statement
+// if ()
+// let count = 0;
+// count += 1;
+
+// // new class to add modal during death // css elem 
+// class Death {
+//     // 
+// }
+
 // data
 const tree = {
     1 : new Screen(
         "escape the labyrinth",
         "please enter coins",
         "https://start-game.carrd.co/assets/images/image01.gif?v=5aed0a9d",
-        new Answer("cancel", 2),
+        new Answer("cancel", 1),
         new Answer("start", 2),
         "cover",
     ),
@@ -40,7 +49,7 @@ const tree = {
         "you find yourself walking towards a new city after a long night of exploration. stay the night or continue the journey", 
         "https://i.pinimg.com/originals/68/31/45/6831454cf213ed7ffa541fe666fa9cf8.gif",
         new Answer("stay", 3), 
-        new Answer("journey", 3), // change num
+        new Answer("journey", 3), // new story change num
         "cover",
     ),
     3 : new Screen(
@@ -48,12 +57,12 @@ const tree = {
         "you walk through the streets and come across this ferris wheel. do you take the ride?",
         "https://giffiles.alphacoders.com/200/200557.gif",
         new Answer("yes", 4),
-        new Answer("no", 4), // change number
+        new Answer("no", 13),
         "cover",
     ),
     4 : new Screen(
         "LEVEL 3",
-        "as you are enjoying the city skyline from atop of the ferris wheel, you notice that their is an alien invasion!",
+        "as you are enjoying the city and its views, you notice that there is an alien invasion!",
         "https://i.pinimg.com/originals/3a/f1/d0/3af1d082c28a0cab1c62004d74258dd2.gif",
         new Answer("jump to escape", 2), // change number
         new Answer("hide in the ferris wheel", 5),
@@ -88,25 +97,55 @@ const tree = {
         "thank goodness you are a great swimmer, they say. get ready for your space walk!",
         "https://i.pinimg.com/originals/71/b0/21/71b021d18cbe5b9f6bbf9f2b8f000d59.gif",
         new Answer("dive in", 2),
-        new Answer("cautiously approach", 2),
+        new Answer("dive in cautiously", 9),
         "cover",
     ),
     9: new Screen(
         "Level 8",
-        "you smash through and instantly are transported back to your childhood home",
-        "",
-        new Answer("keep your distance..", 9), // change number
-        new Answer("enter cautiously", 9),
-        "cover",
+        "you put your ego aside and slowly leave the ship. you notice the raging waters. you ask your friends for help...",
+        "https://cdnb.artstation.com/p/assets/images/images/009/761/481/original/adrian-de-palma-aa1-adrian-de-palma-mockup-pixel-art.gif?1520772604",
+        new Answer("take a submarine", 10), // go to same storyline
+        new Answer("grab scuba gear", 10), // go to same storyline
+        "100% 100%",
     ),
     10: new Screen(
         "Level 9",
-        "You enter your home and observe. Fridge is full, looks lived in. You help yourself. At some point, you here a door open",
-        "",
-        new Answer("test"),
-        new Answer("test")
-    )
+        "whoops! something sucked you into another parallel and you ended up in an underwater world!",
+        "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/52cf024e-4cc2-4480-aa9b-192e14e46400/d9icbyp-154b18c4-7427-452c-924d-e13f6ea182f4.gif?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzUyY2YwMjRlLTRjYzItNDQ4MC1hYTliLTE5MmUxNGU0NjQwMFwvZDlpY2J5cC0xNTRiMThjNC03NDI3LTQ1MmMtOTI0ZC1lMTNmNmVhMTgyZjQuZ2lmIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.I_C8zTZm9bYCNIWUHuqc-IATW2MJE62OMyGyI16zQAQ",
+        new Answer("swim back up to the top!", 2), // restart
+        new Answer("succumb", 11),
+        "cover",
+    ),
+    11: new Screen(
+        "Level 10",
+        "you say your goodbyes as 60s pass, you allow yourself to float. inevitably, you gasp and breathe in. wait huh? you can breathe underwater. you now live your best life underwater",
+        "https://media1.giphy.com/media/JPRruav6eMqd6uBIqr/giphy.gif?cid=6c09b952e6aedb8a96929752e599beb3973e774369401760&rid=giphy.gif&ct=g",
+        new Answer("restart", 1), // restarts game 
+        new Answer("escape", 12),
+        "100% 110%",
+    ),
+    12: new Screen(
+        "\n",
+        "\n",
+        "https://i.gifer.com/OEJZ.gif",
+        new Answer("SRD\nJFK"),
+        new Answer("restart", 1),
+        "100% 100%",
 
+    ),
+    13: new Screen( // new story from begining
+        "Level 3",
+        "you're afraid of heights. but not afraid of psychics. you pop in to get your future read. she suspects an alien abduction is in your cards",
+        "https://www.rookiemag.com/wp-content/uploads/2016/03/1456846171belton1tarot.gif",
+        new Answer("hide from the aliens", 5),
+        new Answer("take this as a joke", 14),
+        "100% 110%"
+    ),
+    14: new Screen( 
+        "Level 4",
+        "you abandon this crazy city and head for your destinaton"
+        
+    )
 };
 
 // execution
@@ -139,14 +178,12 @@ function displayScreen(screen_id) {
     document.body.style.background = "url(" + screen.imgurl + ")";
 
     
-
     // reset button one for the current screen
     let button1 = document.querySelector("#ans1");
     button1.innerText = screen.ans1.text;
 
     // uses the html data-* attribute to create a custom key-value pair on the DOM element that stores the screen_id (see this for more details https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes)
     button1.dataset.screen_id = screen.ans1.screen_id
-
 
     // reset button two for the current screen
     let button2 = document.querySelector("#ans2");
@@ -163,9 +200,16 @@ displayScreen(screen_id);
 
 // Add event listeners to each button once when the page loads (this could be condensed into a single function that works on both buttons)
 button1.addEventListener('click', (e) => {
+    if (button1.innerText == "cancel") {
+        document.body.classList.add('glitch');
+        setTimeout(() => {
+            document.body.classList.remove('glitch');
+        }, 1000);
+    }
     // displayScreen now pulls the screen_id from the DOM element itself, which is set on line 149 for button 1, 156 for button 2
     displayScreen(e.target.dataset.screen_id)
-})
+});
+
 
 button2.addEventListener('click', (e) => {
     displayScreen(e.target.dataset.screen_id)
